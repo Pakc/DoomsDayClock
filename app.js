f2dda6c5c96d02cdb518b513f2a34655e3baf18d
AJ Webb <webbushka@gmail.com>
2015-06-25 15:02:56 -0600

    github page for repo

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function(d, m){
+	let then = m('2015-06-24T22:00:15', 'YYYY-MM-DDTHH:mm:ss');
+
+	setInterval(() => {
+		let now = m(),
+			years = `${now.diff(then, 'years')} Years`,
+			months = `${now.diff(then, 'months') % 12} Months`,
+			// This may result in the wrong number of days... probably a better way of doing this
+			days = `${now.diff(then, 'days') % 30} Days`,
+			hours = `${now.diff(then, 'hours') % 24} Hours`,
+			minutes = `${now.diff(then, 'minutes') % 60} Minutes`,
+			seconds = `${now.diff(then, 'seconds') % 60} Seconds`;
+
+		let el = d.querySelector('#js-moment');
+		el.innerText = `\n${years}\n${months}\n${days}\n${hours}\n${minutes}\n${seconds}!`;
+	}, 1000);
+
+})(document, moment);
